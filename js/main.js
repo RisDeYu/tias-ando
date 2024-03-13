53518ce8947969b6055b354a281803182a49aeee
RisDeYu <92260126+RisDeYu@users.noreply.github.com>
2024-03-14 05:23:20 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+gsap.from('.judul-hero', {duration:1.5, y:100, opacity:0});
+let tl = gsap.timeline(
+  {
+    scrollTrigger:{
+    }
+  }
+);
+
+tl.from('.wrap-arrum',{delay:.5, duration:1, y:100, opacity:0});
+tl.from('.jdl-buka',{delay:.6, duration:1, opacity:0});
+
+
+// album Product Slider-start
+var TrandingSlider = new Swiper('.album-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+// album Product Slider-end
---
+++ js/script.js
@@ -0,0 +1,50 @@
+// Set the date we're counting down to
+var countDownDate = new Date("May 05, 2024 09:30:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  //document.getElementById("hitung-mundur").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  document.getElementById("days").innerText = days,
+  document.getElementById("hours").innerText = hours,
+  document.getElementById("minutes").innerText = minutes,
+  document.getElementById("seconds").innerText = seconds;
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Akad Nikah Telah Dilaksanakan";
+  }
+}, 1000);
+
+function myFunction() {
+  // Copy the text inside the text field
+  navigator.clipboard.writeText("4730954781");
+  
+  // Alert the copied text
+  alert("Ingin menyalin nomor rekening?");
+}
+
+function myFunction1() {
+  
+  // Copy the text inside the text field
+  navigator.clipboard.writeText("a.n Mohammad Ryan Noor Said \n Jl. Otista Raya. Simpang tiga Cimanggis. RT:001/RW:10. No.39. Cimanggis, Ciputat, Kota Tangerang Selatan, Banten. Kode Pos 15411");
+
+  // Alert the copied text
+  alert("Ingin menyalin alamat lengkap?");
+}
+
